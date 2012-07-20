beb72fe83d640b5198f0092bbc5b37e08d7eb722
Matthew King <mking@mking.net>
2012-07-20 09:15:09 +0800

    Added the first few unit tests.

---
+++ JsonFormatterPlus.Tests/JsonFormatterTests.cs
@@ -0,0 +1,24 @@
+﻿namespace JsonFormatterPlus.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal sealed class JsonFormatterTests
+    {
+        [Test]
+        public void Format_JsonIsNull_ThrowsArgumentNullException()
+        {
+            const string message = "json should not be null.";
+            Assert.That(() => JsonFormatter.Format(null),
+                Throws.TypeOf<ArgumentNullException>()
+                      .And.Message.Contains(message));
+        }
+
+        [Test]
+        public void Format_JsonIsEmptyString_ReturnsEmptyString()
+        {
+            Assert.That(JsonFormatter.Format(String.Empty), Is.Empty);
+        }
+    }
+}
--- JsonFormatterPlus/JsonFormatter.cs
+++ JsonFormatterPlus/JsonFormatter.cs
@@ -1,11 +1,17 @@
 ﻿namespace JsonFormatterPlus
 {
+    using System;
     using JsonFormatterPlus.Internals;
 
     public static class JsonFormatter
     {
         public static string Format(string json)
         {
+            if (json == null)
+            {
+                throw new ArgumentNullException("json should not be null.");
+            }
+
             var context = new JsonFormatterStrategyContext();
             var formatter = new JsonFormatterInternal(context);
 
