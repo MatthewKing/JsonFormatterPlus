473cc753ee65909e43444b955029bcd7bd168ce1
Matthew King <mking@mking.net>
2012-08-20 16:19:24 +0800

    Added 'minify' functionality.

--- JsonFormatterPlus/JsonFormatter.cs
+++ JsonFormatterPlus/JsonFormatter.cs
@@ -1,6 +1,7 @@
 ﻿namespace JsonFormatterPlus
 {
     using System;
+    using System.Text.RegularExpressions;
     using JsonFormatterPlus.Internals;
 
     public static class JsonFormatter
@@ -16,6 +17,16 @@
             var formatter = new JsonFormatterInternal(context);
 
             return formatter.Format(json);
+        }
+
+        public static string Minify(string json)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException("json should not be null.");
+            }
+
+            return Regex.Replace(json, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
         }
     }
 }
