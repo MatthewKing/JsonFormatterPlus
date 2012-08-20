b73dc3fb762db4f81c953992cce0ea420c3abd4d
Matthew King <mking@mking.net>
2012-08-20 23:12:01 +0800

    Added XML documentation.

--- JsonFormatterPlus/JsonFormatter.cs
+++ JsonFormatterPlus/JsonFormatter.cs
@@ -4,8 +4,17 @@
     using System.Text.RegularExpressions;
     using JsonFormatterPlus.Internals;
 
+    /// <summary>
+    /// Provides JSON formatting functionality.
+    /// </summary>
     public static class JsonFormatter
     {
+        /// <summary>
+        /// Returns a 'pretty printed' version of the specified JSON string, formatted for human
+        /// consumption.
+        /// </summary>
+        /// <param name="json">A valid JSON string.</param>
+        /// <returns>A 'pretty printed' version of the specified JSON string.</returns>
         public static string Format(string json)
         {
             if (json == null)
@@ -19,6 +28,12 @@
             return formatter.Format(json);
         }
 
+        /// <summary>
+        /// Returns a 'minified' version of the specified JSON string, stripped of all 
+        /// non-essential characters.
+        /// </summary>
+        /// <param name="json">A valid JSON string.</param>
+        /// <returns>A 'minified' version of the specified JSON string.</returns>
         public static string Minify(string json)
         {
             if (json == null)
