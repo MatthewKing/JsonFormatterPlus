7940cf33c97229b7ad354967dff192662180a41e
Matthew King <mking@mking.net>
2012-08-20 16:21:22 +0800

    Incremented version to 1.1.0.

--- JsonFormatterPlus/Properties/AssemblyInfo.cs
+++ JsonFormatterPlus/Properties/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("e39da8f8-48a2-4599-a6af-85a133eb5e23")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
